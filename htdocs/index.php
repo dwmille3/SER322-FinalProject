b47c0038d68d0c8d99568a4d9865e5c368d65c1a
Daniel McEvoy <dmcevoy1@asu.edu>
2016-04-30 16:39:36 -0700

    Added text boxes and radio buttons for input in index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -21,6 +21,28 @@
                 <input type="text" name="text" id="text" placeholder="title/name/theater">
                 <input type="text" name="compare" id="compare" placeholder="title/name/theater">
             </div>
+			
+			<div class="spaced-input">
+				<label for="input">INPUT NEW ELEMENT</label>
+			</div>
+			<form>
+				<input type="radio" name="type" id="theater" value="radio_group">Theater
+				<input type="radio" name="type" id="movie" value="radio_group">Movie
+				<input type="radio" name="type" id="person" value="radio_group">Person
+			</form>
+			<div class="spaced-input">
+				<input type="text" name="title" id="title" placeholder="title/name">
+                <input type="text" name="date1" id="date1" placeholder="Location/ReleaseDate/BirthDate">
+				<input type="text" name="date2" id="date2" placeholder="NoOfTheater/DVDRelease/DeathDate"><br>
+				<input type="text" name="runtime" id="runtime" placeholder="PhoneNo/Runtime/BirthPlace">
+				<input type="text" name="rating" id="rating" placeholder="Blank/Rating/Biography">
+				<input type="text" name="production" id="production" placeholder="Blank/ProductionType/Height"><br>
+				<input type="text" name="genre1" id="genre1" placeholder="Blank/Genre1/Ethnicity">
+				<input type="text" name="genre1" id="genre2" placeholder="Blank/Genre2/Nickname"><br>
+				<input type="text" name="review1" id="review1" placeholder="Blank/Review1/Note">
+				<input type="text" name="review2" id="review2" placeholder="Blank/Review2/Blank">
+			</div>
+			
             <div class="spaced-input">
                 <button name="btnSubmit" type="submit">Submit</button>
                 <button name="btnClear">Clear</button>
